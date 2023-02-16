ce946231d3e442423e206182343e9c2d8e6e59a1
longnhatnguyen <43700676+longnhatnguyen@users.noreply.github.com>
2023-02-16 23:01:32 +0700

    1

---
+++ QLNH/QLNH/Data/book.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QLNH.Data
+{
+    [Table ("book")]
+    public class book
+    {
+        [Key]   
+       public int id { get; set; }
+       public String Title { get; set; }
+        [MaxLength (100)]
+       public String? Descriptions { get; set; }
+        [Range (0, double.MaxValue)]
+       public Double Price { get; set; }
+        [Range (0, 1000)]
+       public int Quantity { get; set; }
+    }
+}
--- QLNH/QLNH/Program.cs
+++ QLNH/QLNH/Program.cs
@@ -6,12 +6,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options => options.AddDefaultPolicy(policy => policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod())); // mo ket noi CORS
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
-{
+{   
     app.UseSwagger();
     app.UseSwaggerUI();
 }
