7d2c47c2196434b8083cd0e11d72b18e66d88238
longnhatnguyen <43700676+longnhatnguyen@users.noreply.github.com>
2023-02-16 21:53:35 +0700

    `

---
+++ QLNH/QLNH/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QLNH.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return null;
+        }
+    }
+}
